12f8c6b4526688a4a21c44632504aa076c73de39
cmhaw <cmhaw@users.noreply.github.com>
2026-06-22 19:53:41 -0500

    Use Netlify checkout backend from GitHub Pages

--- assets/site.js
+++ assets/site.js
@@ -21,7 +21,7 @@ const menuItems = [
 const priceBook = Object.fromEntries(menuItems.map((item) => [item.name, item]));
 const selectedItems = [];
 const bakeHouseEmail = "theakinsbakehouse@yahoo.com";
-const dynamicSquareCheckoutEndpoint = "/api/create-square-checkout";
+const dynamicSquareCheckoutEndpoint = "https://meek-panda-cd04ce.netlify.app/api/create-square-checkout";
 const orderHistoryKey = "akinsBakeHouseOrderHistory";
 const orderCartKey = "akinsBakeHouseCurrentOrder";
 let squareCheckoutInProgress = false;
